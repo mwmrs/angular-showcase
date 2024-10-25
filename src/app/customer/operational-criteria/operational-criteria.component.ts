889660070ce1dfffa9256b8fefc263678df158a0
M. Wiemers <mwmrs@posteo.de>
2024-10-25 13:19:07 +0200

    work on operational criteria and eligibilities

--- src/app/customer/operational-criteria/operational-criteria.component.ts
+++ src/app/customer/operational-criteria/operational-criteria.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, forwardRef } from '@angular/core';
-import { ControlValueAccessor, FormArray, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-operational-criteria',
@@ -16,7 +17,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatIconModule
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [
     {
@@ -26,38 +28,33 @@ import { MatSelectModule } from '@angular/material/select';
     }
   ]
 })
-export class OperationalCriteriaComponent implements ControlValueAccessor {
+export class OperationalCriteriaComponent implements ControlValueAccessor, OnInit {
   form: FormGroup;
   criteriaOptions = ['settlement account', 'custody account', 'pledge account', 'securities account', 'other'];
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      criteria: this.fb.array([])
+    this.form = this.fb.group({});
+    this.criteriaOptions.forEach(option => {
+      this.form.addControl(option, this.fb.control(false));
     });
   }
 
-  get criteria(): FormArray {
-    return this.form.get('criteria') as FormArray;
-  }
-
-  addCriteria() {
-    this.criteria.push(this.fb.group({
-      name: ['', Validators.required],
-      value: [false]
-    }));
-  }
-
-  removeCriteria(index: number) {
-    this.criteria.removeAt(index);
-  }
+  ngOnInit() {}
 
   onTouched: () => void = () => {};
 
   writeValue(val: any): void {
-    if (val) {
-      this.criteria.clear();
+    if (val && this.form) {
+      // Reset all criteria to false
+      this.criteriaOptions.forEach(option => {
+        this.form.get(option)?.setValue(false);
+      });
+
+      // Set the provided values to true
       val.forEach((criterion: any) => {
-        this.criteria.push(this.fb.group(criterion));
+        if (this.form.get(criterion.name)) {
+          this.form.get(criterion.name)?.setValue(true);
+        }
       });
     }
   }
@@ -72,5 +69,11 @@ export class OperationalCriteriaComponent implements ControlValueAccessor {
 
   setDisabledState?(isDisabled: boolean): void {
     isDisabled ? this.form.disable() : this.form.enable();
+  }
+
+  getSelectedCriteria() {
+    return Object.entries(this.form.value)
+      .filter(([_, value]) => value === true)
+      .map(([key, _]) => ({ name: key, value: true }));
   }
 }
--- src/app/customer/operational-eligibilities/operational-eligibilities.component.ts
+++ src/app/customer/operational-eligibilities/operational-eligibilities.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, forwardRef } from '@angular/core';
-import { ControlValueAccessor, FormArray, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-operational-eligibilities',
@@ -18,7 +19,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatIconModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatCheckboxModule
   ],
   providers: [
     {
@@ -28,43 +30,51 @@ import { MatSelectModule } from '@angular/material/select';
     }
   ]
 })
-export class OperationalEligibilitiesComponent implements ControlValueAccessor {
+export class OperationalEligibilitiesComponent implements ControlValueAccessor, OnInit {
   form: FormGroup;
   eligibilityOptions = ['stock_trading', 'margin_trading', 'options_trading', 'futures_trading', 'crypto_trading'];
   measureOptions = ['suspension', 'exclusion', 'limitation'];
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      eligibilities: this.fb.array([])
+      values: this.fb.group({}),
+      details: this.fb.group({})
     });
   }
 
-  get eligibilities(): FormArray {
-    return this.form.get('eligibilities') as FormArray;
-  }
-
-  addEligibility() {
-    this.eligibilities.push(this.fb.group({
-      eligibility_name: ['', Validators.required],
-      value: [false],
-      suspension: [false],
-      start_date: [''],
-      end_date: [''],
-      measure: ['']
-    }));
-  }
-
-  removeEligibility(index: number) {
-    this.eligibilities.removeAt(index);
+  ngOnInit() {
+    this.eligibilityOptions.forEach(option => {
+      (this.form.get('values') as FormGroup).addControl(option, this.fb.control(false));
+      (this.form.get('details') as FormGroup).addControl(option, this.fb.group({
+        start_date: [''],
+        end_date: [''],
+        measure: ['']
+      }));
+    });
   }
 
   onTouched: () => void = () => {};
 
   writeValue(val: any): void {
-    if (val) {
-      this.eligibilities.clear();
+    if (val && this.form) {
+      // Reset all eligibilities to default values
+      this.eligibilityOptions.forEach(option => {
+        this.form.get('values')?.get(option)?.patchValue(false);
+        this.form.get('details')?.get(option)?.patchValue({
+          start_date: '',
+          end_date: '',
+          measure: ''
+        });
+      });
+
+      // Set the provided values
       val.forEach((eligibility: any) => {
-        this.eligibilities.push(this.fb.group(eligibility));
+        if (this.form.get('values')?.get(eligibility.eligibility_name)) {
+          this.form.get('values')?.get(eligibility.eligibility_name)?.patchValue(true);
+        }
+        if (this.form.get('details')?.get(eligibility.eligibility_name)) {
+          this.form.get('details')?.get(eligibility.eligibility_name)?.patchValue(eligibility);
+        }
       });
     }
   }
@@ -79,5 +89,17 @@ export class OperationalEligibilitiesComponent implements ControlValueAccessor {
 
   setDisabledState?(isDisabled: boolean): void {
     isDisabled ? this.form.disable() : this.form.enable();
+  }
+
+  getSelectedEligibilities() {
+    const values = this.form.get('values')?.value;
+    const details = this.form.get('details')?.value;
+    return Object.entries(values)
+      .filter(([_, value]) => value === true)
+      .map(([key, _]) => ({
+        eligibility_name: key,
+        value: true,
+        ...details[key]
+      }));
   }
 }
