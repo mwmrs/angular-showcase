bd354fb2f27f983164f2a4138e599de964580966
M. Wiemers <mwmrs@posteo.de>
2024-12-04 10:28:16 +0100

    make operational-criteria form typed and static

--- src/app/customer/operational-criteria/operational-criteria.component.ts
+++ src/app/customer/operational-criteria/operational-criteria.component.ts
@@ -1,79 +1,96 @@
 import { CommonModule } from '@angular/common';
 import { Component, forwardRef, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+  FormControl,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
+
+interface OperationalCriteriaForm {
+  settlement_account: FormControl<boolean>;
+  custody_account: FormControl<boolean>;
+  pledge_account: FormControl<boolean>;
+  securities_account: FormControl<boolean>;
+  other: FormControl<boolean>;
+}
+
 @Component({
-    selector: 'app-operational-criteria',
-    templateUrl: './operational-criteria.component.html',
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatIconModule,
-        MatCheckboxModule,
-        MatCardModule,
-    ],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => OperationalCriteriaComponent),
-            multi: true,
-        },
-    ]
+  selector: 'app-operational-criteria',
+  templateUrl: './operational-criteria.component.html',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => OperationalCriteriaComponent),
+      multi: true,
+    },
+  ],
 })
 export class OperationalCriteriaComponent implements ControlValueAccessor, OnInit {
-  form: FormGroup;
-  criteriaOptions = ['settlement_account', 'custody_account', 'pledge_account', 'securities_account', 'other'];
+  form: FormGroup<OperationalCriteriaForm>;
+
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({});
-    this.criteriaOptions.forEach((option) => {
-      this.form.addControl(option, this.fb.control(false));
+    this.form = this.fb.group<OperationalCriteriaForm>({
+      settlement_account: new FormControl(false, { nonNullable: true }),
+      custody_account: new FormControl(false, { nonNullable: true }),
+      pledge_account: new FormControl(false, { nonNullable: true }),
+      securities_account: new FormControl(false, { nonNullable: true }),
+      other: new FormControl(false, { nonNullable: true }),
     });
   }
 
   ngOnInit() {}
 
-  onTouched: () => void = () => {};
-
-  writeValue(val: any): void {
-    if (val && this.form) {
-      // Reset all criteria to false
-      this.criteriaOptions.forEach((option) => {
-        this.form.get(option)?.setValue(false);
-      });
-
-      // Set the provided values to true
-      val.forEach((criterion: any) => {
-        if (this.form.get(criterion.name)) {
-          this.form.get(criterion.name)?.setValue(true);
-        }
-      });
+  writeValue(val: Partial<{ [K in keyof OperationalCriteriaForm]: boolean }>): void {
+    if (val) {
+      this.form.patchValue(val, { emitEvent: false });
     }
   }
 
-  registerOnChange(fn: any): void {
-    this.form.valueChanges.subscribe(fn);
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+    this.form.valueChanges.subscribe((value) => {
+      this.onChange(value);
+      this.onTouched();
+    });
   }
 
-  registerOnTouched(fn: any): void {
+  registerOnTouched(fn: () => void): void {
     this.onTouched = fn;
   }
 
-  setDisabledState?(isDisabled: boolean): void {
-    isDisabled ? this.form.disable() : this.form.enable();
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.form.disable({ emitEvent: false });
+    } else {
+      this.form.enable({ emitEvent: false });
+    }
   }
 
-  getSelectedCriteria() {
-    return Object.entries(this.form.value)
-      .filter(([_, value]) => value === true)
-      .map(([key, _]) => ({ name: key, value: true }));
-  }
+  // getSelectedCriteria() {
+  //   return Object.entries(this.form.value)
+  //     .filter(([_, value]) => value === true)
+  //     .map(([key, _]) => ({ name: key, value: true }));
+  // }
 }
